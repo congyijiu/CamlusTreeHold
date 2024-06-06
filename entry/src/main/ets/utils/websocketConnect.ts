b6e326b027b6dd379bc85a8c235d70efdcaf9f63
hulinfeng <116654493+clcikcode@users.noreply.github.com>
2024-06-06 22:21:47 +0800

    MessagePage.ets

--- entry/src/main/ets/utils/websocketConnect.ts
+++
@@ -1,8 +0,0 @@
-import webSocket from '@ohos.net.webSocket';
-
-export function websocketConnect(onMessage: (data: string) => void) {
-  var defaultIpAddress = "ws://http://192.168.31.196:8080/websocket/";
-  let ws = webSocket.createWebSocket();
-  ws.connect(defaultIpAddress)
-
-}
